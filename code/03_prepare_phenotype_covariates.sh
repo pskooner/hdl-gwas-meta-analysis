f1a91f513ad35e7ed50b92c8c66791289de7019d
Parminder Singh Kooner <pkooner3@uwo.ca>
2026-08-25 15:12:56 -0500

    Add script to prepare phenotype and covariate data
    Extract phenotype and covariate data from lab4_pheno_gwas.csv and save to hdl.pheno and hdl.covar.

--- code/03_prepare_phenotype_covariates.sh
+++ code/03_prepare_phenotype_covariates.sh
@@ -1,1 +1,13 @@
+head lab4_pheno_gwas.csv
 
+awk -F, '{print $1, $1, $4}' lab4_pheno_gwas.csv | head
+
+awk -F, '{print $1, $1, $4}' lab4_pheno_gwas.csv | sed -e '1d' | head
+
+awk -F, '{print $1, $1, $4}' lab4_pheno_gwas.csv | sed -e '1d' > hdl.pheno
+
+head hdl.pheno
+
+awk -F, '{print $1, $1, $2, $3}' lab4_pheno_gwas.csv | sed 's/F/2/g' | sed 's/M/1/g' | sed -e '1d' > hdl.covar
+
+head hdl.covar
