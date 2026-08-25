623747ad5f6b6835a708d40abbee8e33c454b6dc
Parminder Singh Kooner <pkooner3@uwo.ca>
2026-08-25 15:10:06 -0500

    Implement genotype QC steps in 02_genotype_qc.sh
    Added commands for genotype quality control including missingness, minor allele frequency, and Hardy-Weinberg equilibrium checks.

--- code/02_genotype_qc.sh
+++ code/02_genotype_qc.sh
@@ -1,1 +1,38 @@
+# Q2 Individual missingness
+plink --noweb --file lab4 --missing --out lab4_miss
 
+head lab4_miss.imiss
+
+wc lab4_miss.imiss
+
+sort -n -r -k6 lab4_miss.imiss | head
+
+
+# Q3 SNP missingness
+head lab4_miss.lmiss
+
+wc lab4_miss.lmiss
+
+sort -n -r -k5 lab4_miss.lmiss | head
+
+
+# Q4 Minor allele frequency
+plink --noweb --file lab4 --freq --out lab4_frq
+
+head lab4_frq.frq
+
+sort -n -k5 lab4_frq.frq | head
+
+sort -n -k5 lab4_frq.frq | awk '{if($5 > 0) print}' | head
+
+
+# Q4 Hardy-Weinberg equilibrium
+plink --noweb --file lab4 --hardy --out lab4_hardy
+
+head lab4_hardy.hwe
+
+grep ALL lab4_hardy.hwe | head
+
+grep ALL lab4_hardy.hwe | sort -n -k9 | head
+
+grep ALL lab4_hardy.hwe | awk '{if (NF == 9) print}' | sort -n -k9 | head
